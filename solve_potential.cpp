cec6a25731830e8d4b1136482666de401c11498a
VictorDelatour <hugo.babel@epfl.ch>
2015-04-24 14:14:09 +0200

    Trying to pass the world by reference doesn't change anything.

--- solve_potential.cpp
+++ solve_potential.cpp
@@ -1,35 +1,35 @@
 #include "solve_potential.hpp"
 
-void solve_potential(World *world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& potential){
+void solve_potential(World& world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& potential){
 	
 	
 	real_function_3d rho_interp;
 	// real_function_3d phi;
 	
-	if (world->rank() == 0) printf("Setup initial parameters\n");
+	if (world.rank() == 0) printf("Setup initial parameters\n");
 	set_initial_parameters(nx);
-	if (world->rank() == 0) printf("Set...\n\n");
+	if (world.rank() == 0) printf("Set...\n\n");
 	
-	if (world->rank() == 0) printf("Setup projection precision\n");
+	if (world.rank() == 0) printf("Setup projection precision\n");
 	set_projection_precision(9, 1e-7);
-	if (world->rank() == 0) printf("Set...\n\n");
+	if (world.rank() == 0) printf("Set...\n\n");
 	
-	if (world->rank() == 0) printf("Build projected density\n");
+	if (world.rank() == 0) printf("Build projected density\n");
 	build_projected_density(world, nx, ny, nz, density, rho_interp);
-	if (world->rank() == 0) printf("Built...\n\n");
+	if (world.rank() == 0) printf("Built...\n\n");
 	
-	if (world->rank() == 0) printf("Printing density\n");
+	if (world.rank() == 0) printf("Printing density\n");
 	print_density(world, rho_interp, 128, nx);
-	if (world->rank() == 0) printf("Printed...\n\n");
+	if (world.rank() == 0) printf("Printed...\n\n");
 
-	// CORE DUMP? FUCK YOU
-	if (world->rank() == 0) printf("Computing potential\n");
+	// CORE DUMP? WHY?
+	if (world.rank() == 0) printf("Computing potential\n");
 	compute_potential(world, rho_interp, potential, 1e-6, 1e-8);
-	if (world->rank() == 0) printf("Computed...\n\n");
+	if (world.rank() == 0) printf("Computed...\n\n");
 	
-	// if (world->rank() == 0) printf("Printing potential\n");
+	// if (world.rank() == 0) printf("Printing potential\n");
 	// print_potential(world, potential, 128, nx);
-	// if (world->rank() == 0) printf("Printed...\n\n");
+	// if (world.rank() == 0) printf("Printed...\n\n");
 	
 }
 
@@ -52,44 +52,70 @@ void set_projection_precision(const int& order, const double& threshold){
 	
 }
 
-void build_projected_density(World *world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& projected_density){
+void build_projected_density(World& world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& projected_density){
 	
 	real_functor_3d density_functor;
 	
 	density_functor = real_functor_3d(new DensityProjector(nx, ny, nz, &density[0]));
-	projected_density = real_factory_3d(*world).functor(density_functor);
+	projected_density = real_factory_3d(world).functor(density_functor);
 	
-	// if (world->rank() == 0) printf("Density evaluation: %f\n", projected_density(5.0, 5.0, 5.0));
+	// if (world.rank() == 0) printf("Density evaluation: %f\n", projected_density(5.0, 5.0, 5.0));
 	
 }
 
-void compute_potential(World* world, real_function_3d& projected_density, real_function_3d& potential, const double& precision, const double& threshold){
+void compute_potential(World& world, real_function_3d& projected_density, real_function_3d& potential, const double& precision, const double& threshold){
 	
 	double integral, volume, mean;
-	real_function_3d yolo;
+	// real_function_3d yolo;
 
-	if (world->rank() == 0) printf("\tProjecting potential\n");
+	if (world.rank() == 0) printf("\tProjecting potential\n");
 	
-	real_convolution_3d coulomb_operator = CoulombOperator(*world, precision, threshold);
+	real_convolution_3d coulomb_operator = CoulombOperator(world, precision, threshold);
 	
 	// This is the problem...
-	//potential = coulomb_operator(projected_density);
+	try{
+		potential = coulomb_operator(projected_density);
+	}
+	catch (const SafeMPI::Exception& e) {
+        //print(e);
+        error("caught an MPI exception");
+    } catch (const madness::MadnessException& e) {
+        print(e);
+        error("caught a MADNESS exception");
+    } catch (const madness::TensorException& e) {
+        print(e);
+        error("caught a Tensor exception");
+    } catch (const char* s) {
+        print(s);
+        error("caught a c-string exception");
+    } catch (char* s) {
+        print(s);
+        error("caught a c-string exception");
+    } catch (const std::string& s) {
+        print(s);
+        error("caught a string (class) exception");
+    } catch (const std::exception& e) {
+        print(e.what());
+        error("caught an STL exception");
+    } catch (...) {
+        error("caught unhandled exception");
+    }
 	
 	// yolo = coulomb_operator(projected_density);
 	
-	if (world->rank() == 0) printf("\tProjected potential\n");
+	if (world.rank() == 0) printf("\tProjected potential\n");
 	//
 	// integral = potential.trace();
 	// volume = FunctionDefaults<3>::get_cell_volume();
 	// mean = integral/volume;
 	//
 	// potential = potential - mean;
-	// if (world->rank() == 0) printf("\tNormalized\n");
+	// if (world.rank() == 0) printf("\tNormalized\n");
 	
 }
 
 
-void print_density(World *world, const real_function_3d& projected_density, const int& numpts, const int& nx){
+void print_density(World& world, const real_function_3d& projected_density, const int& numpts, const int& nx){
 	
 	// Check if the density has been projected!
 	
@@ -105,13 +131,13 @@ void print_density(World *world, const real_function_3d& projected_density, cons
 		npoints[i] = numpts;
 	}
 	
-	plotvtk_begin(*world, filename_density, plotlo, plothi, npoints);
-	plotvtk_data(projected_density, "density", *world, filename_density, plotlo, plothi, npoints);
-	plotvtk_end<3>(*world, filename_density);
+	plotvtk_begin(world, filename_density, plotlo, plothi, npoints);
+	plotvtk_data(projected_density, "density", world, filename_density, plotlo, plothi, npoints);
+	plotvtk_end<3>(world, filename_density);
 	
 }
 
-void print_potential(World* world, const real_function_3d& potential, const int& numpts, const int& nx){
+void print_potential(World& world, const real_function_3d& potential, const int& numpts, const int& nx){
 	
 	const char filename_potential[] = "data/spartan_potential.vts";
 	
@@ -125,8 +151,8 @@ void print_potential(World* world, const real_function_3d& potential, const int&
 		npoints[i] = numpts;
 	}
 	
-	plotvtk_begin(*world, filename_potential, plotlo, plothi, npoints);
-	plotvtk_data(potential, "potential", *world, filename_potential, plotlo, plothi, npoints);
-	plotvtk_end<3>(*world, filename_potential);
+	plotvtk_begin(world, filename_potential, plotlo, plothi, npoints);
+	plotvtk_data(potential, "potential", world, filename_potential, plotlo, plothi, npoints);
+	plotvtk_end<3>(world, filename_potential);
 	
 }
--- solve_potential.hpp
+++ solve_potential.hpp
@@ -1,15 +1,18 @@
 #include <vector>
-#include "density_projector3d.hpp"
+
 #include <madness/mra/mra.h>
+#include <madness/mra/operator.h>
+
+#include "density_projector3d.hpp"
 
 typedef double real_t;
 
 using namespace madness;
 
-void solve_potential(World *world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& potential);
+void solve_potential(World& world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& potential);
 void set_initial_parameters(const int& nx);
 void set_projection_precision(const int& order, const double& threshold);
-void build_projected_density(World *world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& projected_density);
-void compute_potential(World* world, real_function_3d& projected_density, real_function_3d& potential, const double& precision, const double& threshold);
-void print_density(World* world, const real_function_3d& projected_density, const int& numpts, const int& nx);
-void print_potential(World* world, const real_function_3d& potential, const int& numpts, const int& nx);
+void build_projected_density(World& world, const int& nx, const int& ny, const int& nz, real_t* density, real_function_3d& projected_density);
+void compute_potential(World& world, real_function_3d& projected_density, real_function_3d& potential, const double& precision, const double& threshold);
+void print_density(World& world, const real_function_3d& projected_density, const int& numpts, const int& nx);
+void print_potential(World& world, const real_function_3d& potential, const int& numpts, const int& nx);
